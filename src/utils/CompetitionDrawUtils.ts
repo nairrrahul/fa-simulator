d7d6c3cd818541df744e9c8788b8ef977b46212a
Rahul Nair <nair.rahul992@gmail.com>
2025-12-19 18:12:10 -0800

    group scheduling skeleton

--- src/utils/CompetitionDrawUtils.ts
+++ src/utils/CompetitionDrawUtils.ts
@@ -6,7 +6,7 @@ import nlCompInfo from '../data/competitions/nations_league.json'  with { type: 
 import qualifiersCompInfo from '../data/competitions/qualifying_competitions.json'  with { type: 'json' };
 import windowDates from '../data/competitions/confederation_windows.json'  with { type: 'json' };
 import optionalWindowDates from '../data/competitions/optional_windows.json'  with { type: 'json' };
-import { FinalsCompetitionJSON, GroupStageQualsRound, HomeAwayQualsRound, NationsLeagueJSON, QualifyingStageJSON } from "../common/competitions.interfaces";
+import { FinalsCompetitionJSON, GroupStageQualsRound, HomeAwayQualsRound, NationsLeagueCompetitionStruct, NationsLeagueJSON, QualifyingStageJSON } from "../common/competitions.interfaces";
 import { getRoundInfoForCompetition } from "./CompetitionFormatUtils";
 
 const FINALS_JSON = finalsCompInfo as FinalsCompetitionJSON;
@@ -170,14 +170,80 @@ export function generateHomeAwayFixtures(pairs: [Nation, Nation][], compId: numb
 }
 
 export function getQNLGroupWindows(teamsInGroup: number, compId: number, compType: number, roundId: number, year: number): number[][] {
-  const roundInfo = getRoundInfoForCompetition(compId, teamsInGroup, roundId) as GroupStageQualsRound;
-  const relativeYear = compType == 1 ? QUAL_JSON[compId.toString()].relativeYear : NL_JSON[compId.toString()].relativeYear;
-  const indexToAccess = OPTIONAL_WINDOWS[compId.toString()][teamsInGroup.toString()];
-  const mandatoryWindows = roundInfo.mandatoryWindows;
-  const optionalWindowsToAdd = roundInfo.optionalWindows.slice(0, indexToAccess+1);
-  const windows: number[][] = [...mandatoryWindows, ...optionalWindowsToAdd];
-  //for each window in windows, we need to change the year to be the proper year for the fixture.
-  //the way we do this is by seeing 
-  const retWindows = windows.map(window => [window[0], window[1] + (year - relativeYear)]);
-  return retWindows;
+  if(compType == 1) {
+    const roundInfo = getRoundInfoForCompetition(compId, teamsInGroup, roundId) as GroupStageQualsRound;
+    const relativeYear = QUAL_JSON[compId.toString()].relativeYear;
+    const indexToAccess = OPTIONAL_WINDOWS[compId.toString()][teamsInGroup.toString()];
+    const mandatoryWindows = roundInfo.mandatoryWindows;
+    const optionalWindowsToAdd = roundInfo.optionalWindows.slice(0, indexToAccess+1);
+    const windows: number[][] = [...mandatoryWindows, ...optionalWindowsToAdd];
+    const retWindows = windows.map(window => [window[0], window[1] + (year - relativeYear)]);
+    return retWindows;
+  } else {
+    const nlInfo = NL_JSON[compId.toString()] as NationsLeagueCompetitionStruct;
+    const relativeYear = nlInfo.relativeYear;
+    const windows = nlInfo.groupWindows;
+    const retWindows = windows.map(window => [window[0], window[1] + (year - relativeYear)]);
+    return retWindows;
+  }
+}
+
+export function generateGroupFixtures(group: CompetitionGroup[], compId: number, returnEnabled: boolean) : Record<number,Fixture[]> {
+  const teamsInGroup: number[] = group.map(g => g.teamID);
+  if(teamsInGroup.length % 2 == 1) {
+    teamsInGroup.push(-1); //add dummy team to make it even
+  }
+
+  const fixtures: Record<number,Fixture[]> = {};
+  const iterations = returnEnabled ? 2*(teamsInGroup.length - 1) : teamsInGroup.length - 1;
+  const half = Math.floor(teamsInGroup.length / 2);
+
+  for(let i = 0; i < iterations; i++) {
+    
+    if(i < teamsInGroup.length - 1) {
+      let matchups: Fixture[] = [];
+      for(let j = 0; j < half; j++) {
+        let team1 = teamsInGroup[j];
+        let team2 = teamsInGroup[teamsInGroup.length - 1 - j];
+        if(team1 != -1 && team2 != -1) {
+          matchups.push({
+            id: -1,
+            team1ID: team1,
+            team2ID: team2,
+            competitionID: compId,
+            groupID: group[0].groupID,
+            roundID: group[0].roundID,
+            date: "",
+            scoreline: null,
+            outcome: null
+          })
+        }
+      }
+      fixtures[i] = matchups;
+      const cycleOut = teamsInGroup.pop();
+      teamsInGroup.splice(1, 0, cycleOut!)
+    } else {
+      fixtures[i] = fixtures[i - teamsInGroup.length + 1].map(fixture => ({
+        ...fixture,
+        team1ID: fixture.team2ID,
+        team2ID: fixture.team1ID
+      }));
+    }
+    
+  }
+
+  let initIdx = 1;
+  const retFixtures: Record<number,Fixture[]> = {};
+  for(const [key, value] of Object.entries(fixtures)) {
+    retFixtures[parseInt(key)] = value.map(fixture => ({
+      ...fixture,
+      id: initIdx++
+    }));
+  }
+
+  return retFixtures;
+}
+
+export function assignDatesToQNLGroupFixtures(fixtures: Fixture[], compType: number, year: number): Fixture[] {
+  return fixtures;
 }
